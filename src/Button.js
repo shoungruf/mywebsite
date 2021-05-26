97786d80b9d3b042391455cdfc047624d50cb5a5
Shoungruf Raina <shoungrufraina@Shoungrufs-Space.local>
2021-05-26 23:25:11 +0530

    switch/links

---
+++ src/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class Button extends Component {
+  render() {
+    
+    return <img src={logo} className="App-logo" alt="logo" />;
+    // ...
+  }
+}
+
+export default Button; 
