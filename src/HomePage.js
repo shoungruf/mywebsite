78385a5b71fea83e578046577b98b3387c470818
Shoungruf Raina <polar.sphere.9@gmail.com>
2021-06-03 21:26:22 +0530

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,54 +1,3 @@
-// import React, { Component } from 'react';
-// import './App.css';
-// import 'bootstrap/dist/css/bootstrap.css';
-
-// import FrameSliderButtons from './FrameSliderButtons';
-// // import AboutPage from './AboutPage';
-// // import WorkPage from './WorkPage';
-// // import ResumePage from './ResumePage';
-// // import BlogPage from './BlogPage';
-// // import ContactPage from './ContactPage';
-
-// class HomePage extends Component {
-//   render() {
-//     return (
-//       <div className='App-Frame'>
-//         <div class="container">
-
-//           <div class="col-md-1" >
-//             <div class="line"></div>
-//           </div>
-//           <div class="col-md-6" >
-//             <div class="container">
-//               <div class="row">
-//                 <div class="col-md-3 marginNeg2em" >
-//                   {/* {/* TODO: Frame slider buttons not in line */}
-//                   <FrameSliderButtons />
-
-//                 </div>
-//                 <div class="col-md-4">
-//                   <text className="TitleClass" > Shoungruf Raina </text>
-//                   <text className="TitleClassDesc"> Designer and Developer</text>
-//                 </div>
-//               </div>
-//               {/* <div class="row row-invisible">
-//               <FrameSliderButtons />
-//                 </div> */}
-// <div class="col-md-1" >
-//             <div class="line"></div>
-//           </div>
-//         </div>
-
-//       </div>
-//       </div>
-//       </div>
-
-//     );
-//   }
-// }
-// export default HomePage;
-
-
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -56,12 +5,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import FrameSliderButtons from './FrameSliderButtons';
 import FrameSliderBottomButton from './FrameSliderBottomButton';
 import dp from "./SliderButtons/IMG_1666.jpeg"
-
-// import AboutPage from './AboutPage';
-// import WorkPage from './WorkPage';
-// import ResumePage from './ResumePage';
-// import BlogPage from './BlogPage';
-// import ContactPage from './ContactPage';
 
 class HomePage extends Component {
   render() {
@@ -81,7 +24,7 @@ class HomePage extends Component {
                   <FrameSliderButtons />
 
                 </div>
-                <div class="col-md-7">
+                <div class="col-md-9 centerAlign">
                   <text className="TitleClass" > Shoungruf Raina </text>
                  
                   <text className="TitleClassDesc"> Designer and Developer</text>
